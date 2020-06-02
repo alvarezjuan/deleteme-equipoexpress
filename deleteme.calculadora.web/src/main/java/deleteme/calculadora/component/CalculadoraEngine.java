ed9f0f201d7fc07e3cfab65d5f5f9c98ad63f0c1
Isaac Díaz <lautaromantis@gmail.com>
2020-06-02 18:44:58 -0500

    funcionalidad basica de CalculadoraEngine
    agrega suma, resta, multiplicacion y division

--- deleteme.calculadora.web/src/main/java/deleteme/calculadora/component/CalculadoraEngine.java
+++ deleteme.calculadora.web/src/main/java/deleteme/calculadora/component/CalculadoraEngine.java
@@ -3,6 +3,48 @@ package deleteme.calculadora.component;
 public class CalculadoraEngine {
     
     public String Calcular(String primerOperando, String operador, String segundoOperando, String calcular) {
-        return "Adicionar el codigo aqui de calculo";
+
+        String resultado;
+
+        Integer op1 = Integer.parseInt(primerOperando);
+        Integer op2 = Integer.parseInt(segundoOperando);
+
+        switch (operador) {
+            case "add":
+                resultado = sumar(op1, op2);
+                break;
+            case "substract":
+                resultado = restar(op1, op2);
+                break;
+
+            case "multiply":
+                resultado = multiplicar(op1, op2);
+                break;
+
+            case "divide":
+                resultado = dividir(op1, op2);
+                break;
+            default:
+                resultado = "La operacion "+operador+" no está soportada.";
+                break;
+        }
+
+        return resultado;
+    }
+
+    public String sumar(Integer op1, Integer op2){
+       return String.valueOf((op1 + op2));
+    }
+
+    public String restar(Integer op1, Integer op2){
+        return String.valueOf((op1 - op2));
+    }
+
+    public String multiplicar(Integer op1, Integer op2){
+        return String.valueOf((op1 * op2));
+    }
+
+    public String dividir(Integer op1, Integer op2){
+        return String.valueOf((op1 / op2));
     }
 }
